3bb8390a0dbfa6923900d84b6c6e856746e586cb
acer <wongsatorn1501003@gmail.com>
2019-10-15 21:18:48 +0700

    8

--- app/src/main/java/com/example/buttoncilckapp/MainActivity.kt
+++ app/src/main/java/com/example/buttoncilckapp/MainActivity.kt
@@ -9,26 +9,21 @@ import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    private var userInput: EditText? = null
-    private var button: Button? = null
     private var textView: TextView? = null
-    private var numTimesClicked = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        userInput = findViewById<EditText>(R.id.editText)
-        button = findViewById<Button>(R.id.button)
-        textView= findViewById<TextView>(R.id.textView2)
+        val userInput = findViewById<EditText>(R.id.editText)
+         val button = findViewById<Button>(R.id.button)
+        textView = findViewById<TextView>(R.id.textView2)
         textView?.movementMethod = ScrollingMovementMethod()
 
-        button?.setOnClickListener {object :View.OnClickListener {
-            override fun onClick(p0: View?) {
-                numTimesClicked += 1
-                textView?.append("\nThe button got tapped $numTimesClicked time")
-                if(numTimesClicked != 1) {
-                    textView?.append("s")
-                    }
+        button?.setOnClickListener {object : View.OnClickListener {
+                override fun onClick(p0: View?) {
+                    textView?.append( userInput?.text)
+                    textView?.append("\n")
                 }
             }
         }
