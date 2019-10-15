807f01c37c17161e4916417d7f578af687f4a638
acer <wongsatorn1501003@gmail.com>
2019-10-15 21:06:23 +0700

    5

--- app/src/main/java/com/example/buttoncilckapp/MainActivity.kt
+++ app/src/main/java/com/example/buttoncilckapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.buttoncilckapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -10,6 +11,7 @@ class MainActivity : AppCompatActivity() {
     private var userInput: EditText? = null
     private var button: Button? = null
     private var textView: TextView? = null
+    private var numTimesClicked = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,5 +19,13 @@ class MainActivity : AppCompatActivity() {
         userInput = findViewById<EditText>(R.id.editText)
         button = findViewById<Button>(R.id.button)
         textView= findViewById<TextView>(R.id.textView2)
+
+        button?.setOnClickListener {object :View.OnClickListener {
+            override fun onClick(p0: View?) {
+                numTimesClicked += 1
+                textView?.append("\nThe button got tapped $numTimesClicked time")
+                }
+            }
+        }
     }
 }
