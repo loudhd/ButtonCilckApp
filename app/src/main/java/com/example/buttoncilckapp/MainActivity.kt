efe179e82e833540b490bee6c5ba947ca6323a4d
acer <wongsatorn1501003@gmail.com>
2019-10-15 21:00:26 +0700

    4

--- app/src/main/java/com/example/buttoncilckapp/MainActivity.kt
+++ app/src/main/java/com/example/buttoncilckapp/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.buttoncilckapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-
+    private var userInput: EditText? = null
+    private var button: Button? = null
+    private var textView: TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        userInput = findViewById<EditText>(R.id.editText)
+        button = findViewById<Button>(R.id.button)
+        textView= findViewById<TextView>(R.id.textView2)
     }
 }
