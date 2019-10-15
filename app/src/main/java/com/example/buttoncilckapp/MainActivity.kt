0e136476d27100f135de6de59bc7b83745f3d031
acer <wongsatorn1501003@gmail.com>
2019-10-15 21:11:34 +0700

    6

--- app/src/main/java/com/example/buttoncilckapp/MainActivity.kt
+++ app/src/main/java/com/example/buttoncilckapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.buttoncilckapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -19,11 +20,15 @@ class MainActivity : AppCompatActivity() {
         userInput = findViewById<EditText>(R.id.editText)
         button = findViewById<Button>(R.id.button)
         textView= findViewById<TextView>(R.id.textView2)
+        textView?.movementMethod = ScrollingMovementMethod()
 
         button?.setOnClickListener {object :View.OnClickListener {
             override fun onClick(p0: View?) {
                 numTimesClicked += 1
                 textView?.append("\nThe button got tapped $numTimesClicked time")
+                if(numTimesClicked != 1) {
+                    textView?.append("s")
+                    }
                 }
             }
         }
